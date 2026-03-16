9100bd0328d3d0326b92cbd1966f1faf78978e69
Lauzari <mlauramoyano1985@gmail.com>
2026-03-16 09:08:10 -0300

    start version

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 
 import './App.css'
+import responsemovies from './mocks/with-results.json'
+import withoutResults from './mocks/with-results.json'
 
-function App() {
-return(
-  <header>
-    
+function App (){
+  const movies= responsemovies.Search
+  const hasMovies= movies?.length>0 
+  return (
+    <div className='page'>
+      
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className='form'>
+          <input placeholder='Avengers, Star Wars, The MaTrix...'/>
+          <button type= 'submit'>Buscar</button>
+        </form>
+      </header>
+  <main> 
+   { hasMovies ? (
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.imdbID}>
+            <h3>{movie.Title}</h3>
+            <p>{movie.Year}</p>
+            <img src={movie.Poster} alt={movie.Title}/>
+          </li>
 
-  </header>
-  <div>
-    <form className='form'form>
-  </div>
-)
+        ))
+        }
+      </ul>
+   ) : ( 
+    <p>No se encontraron resultados para esta busqueda</p>
 
+      )}
+  </main>
+    </div>
+  )
 }
-export default App
+  export default App
